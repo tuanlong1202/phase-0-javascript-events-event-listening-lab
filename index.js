386a6fc34bac5025dc3e8a4cdb80efdf07db511d
tuanlong1202 <tuanlong1202@gmail.com>
2021-03-25 21:19:44 -0400

    Phase 0 javascript event listening DONE

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+
+const input = document.getElementById('input');
+
+function clickAlert() {
+    alert('Hee hee, that tickles!');
+}
+
 function addingEventListener() {
+    //
+    alert('Hoo hoo, that tickles!');
+    input.addEventListener('click', clickAlert);
 }
